054f673c0217f8de7cd2fc120494d4fadf329b14
Dima <dimafrontbackdev@gmail.com>
2022-12-04 18:54:31 +0200

    qwe

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,16 +6,21 @@ window.onbeforeunload = function () {
 // find elements
 const tunnelShadowWrapper = document.querySelector('.tunnel-shadow-wrapper');
 const introBgTunnelShadow = document.querySelector('.intro__bg-tunnel-shadow');
+const introRow = document.querySelector('.intro__row');
 
 const tunnelShadowRectTop = introBgTunnelShadow.getBoundingClientRect().top; // position of tunnel in the page
+const introRowRectTop = introRow.getBoundingClientRect().top; // position of introRow in the page
 const delay = window.innerWidth > 1441 ? 300 : window.innerWidth > 1025 ? 200 : 100; // delay before starting animation
 let wrapperHeight = introBgTunnelShadow.clientHeight;
+let introRowHeight = introRow.clientHeight;
 let windowScrollYMorePrevValue = 0;
 
-console.log(introBgTunnelShadow.clientHeight);
+// initial height for tunnelShadowWrapper
+tunnelShadowWrapper.style.height = `${
+  wrapperHeight - (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
+}px`;
 
 window.addEventListener('scroll', () => {
-  console.log(window.scrollY);
   // if scroll down
   if (window.scrollY > windowScrollYMorePrevValue) {
     windowScrollYMorePrevValue = window.scrollY;
@@ -33,6 +38,23 @@ window.addEventListener('scroll', () => {
       tunnelShadowWrapper.style.height = `${
         wrapperHeight - (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
       }px`;
+
+      // if we scroll below introRow, we hide shadow
+      if (-(introRowRectTop - window.scrollY - window.innerHeight) - introRowHeight > 0) {
+        tunnelShadowWrapper.style.transform = `translateY(${
+          (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1 +
+          wrapperHeight -
+          (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
+        }px)`;
+        // tunnel shadow go to top
+        introBgTunnelShadow.style.transform = `translateY(-${
+          (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1 +
+          wrapperHeight -
+          (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
+        }px)`;
+
+        tunnelShadowWrapper.style.height = `0px`;
+      }
     }
   }
 });
