687244cce2a9c9861c72175299e4ccf7f4d0e6f6
Dima <dimafrontbackdev@gmail.com>
2022-12-04 14:42:02 +0200

    qwe

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,1 +1,32 @@
-!function(){const n=document.querySelector(".tunnel-shadow-wrapper"),e=document.querySelector(".intro__bg-tunnel-shadow"),t=e.getBoundingClientRect().top,o=300;window.addEventListener("scroll",(()=>{-(t-window.scrollY-window.innerHeight)-o>0&&(n.style.transform=`translateY(${-1*(t-window.scrollY-window.innerHeight+o)}px)`,e.style.transform=`translateY(-${-1*(t-window.scrollY-window.innerHeight+o)}px)`,n.style.height=1762.5- -1*(t-window.scrollY-window.innerHeight+o)+"px")}))}();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/assets/js/app.js":
+/*!******************************!*\
+  !*** ./src/assets/js/app.js ***!
+  \******************************/
+/***/ (function() {
+
+eval("// for proper work of script of animation of disappear tunnel shadow\r\nwindow.onbeforeunload = function () {\r\n  window.scrollTo(0, 0);\r\n};\r\n\r\n// find elements\r\nconst tunnelShadowWrapper = document.querySelector('.tunnel-shadow-wrapper');\r\nconst introBgTunnelShadow = document.querySelector('.intro__bg-tunnel-shadow');\r\n\r\nconst tunnelShadowRectTop = introBgTunnelShadow.getBoundingClientRect().top; // position of tunnel in the page\r\nconst delay = window.innerWidth > 1441 ? 300 : window.innerWidth > 1025 ? 200 : 100; // delay before starting animation\r\nlet wrapperHeight = introBgTunnelShadow.clientHeight;\r\nlet windowScrollYMorePrevValue = 0;\r\n\r\nconsole.log(introBgTunnelShadow.clientHeight);\r\n\r\nwindow.addEventListener('scroll', () => {\r\n  console.log(window.scrollY);\r\n  // if scroll down\r\n  if (window.scrollY > windowScrollYMorePrevValue) {\r\n    windowScrollYMorePrevValue = window.scrollY;\r\n    // if we scroll tunnel + delay\r\n    if (-(tunnelShadowRectTop - window.scrollY - window.innerHeight) - delay > 0) {\r\n      // tunnel shadow wrapper go to bottom\r\n      tunnelShadowWrapper.style.transform = `translateY(${\r\n        (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1\r\n      }px)`;\r\n      // tunnel shadow go to top\r\n      introBgTunnelShadow.style.transform = `translateY(-${\r\n        (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1\r\n      }px)`;\r\n      // decrease tunnelShadowWrapper height because it's will be climb out of the page\r\n      tunnelShadowWrapper.style.height = `${\r\n        wrapperHeight - (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1\r\n      }px`;\r\n    }\r\n  }\r\n});\r\n\n\n//# sourceURL=webpack://brainscloud/./src/assets/js/app.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/assets/js/app.js"]();
+/******/ 	
+/******/ })()
+;
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,23 +1,38 @@
+// for proper work of script of animation of disappear tunnel shadow
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
+
+// find elements
 const tunnelShadowWrapper = document.querySelector('.tunnel-shadow-wrapper');
 const introBgTunnelShadow = document.querySelector('.intro__bg-tunnel-shadow');
-const tunnelShadowRectTop = introBgTunnelShadow.getBoundingClientRect().top;
-const delay = 300;
-let height = 1762.5;
+
+const tunnelShadowRectTop = introBgTunnelShadow.getBoundingClientRect().top; // position of tunnel in the page
+const delay = window.innerWidth > 1441 ? 300 : window.innerWidth > 1025 ? 200 : 100; // delay before starting animation
+let wrapperHeight = introBgTunnelShadow.clientHeight;
+let windowScrollYMorePrevValue = 0;
+
+console.log(introBgTunnelShadow.clientHeight);
 
 window.addEventListener('scroll', () => {
-  //   console.log('tunnelShadowRectTop: ' + tunnelShadowRectTop);
-  //   console.log('window.scrollY: ' + Number(window.scrollY) - Number(window.innerHeight));
-
-  if (-(tunnelShadowRectTop - window.scrollY - window.innerHeight) - delay > 0) {
-    tunnelShadowWrapper.style.transform = `translateY(${
-      (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
-    }px)`;
-    introBgTunnelShadow.style.transform = `translateY(-${
-      (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
-    }px)`;
-    tunnelShadowWrapper.style.height = `${
-      height - (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
-    }px`;
-    //  console.log((tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1);
+  console.log(window.scrollY);
+  // if scroll down
+  if (window.scrollY > windowScrollYMorePrevValue) {
+    windowScrollYMorePrevValue = window.scrollY;
+    // if we scroll tunnel + delay
+    if (-(tunnelShadowRectTop - window.scrollY - window.innerHeight) - delay > 0) {
+      // tunnel shadow wrapper go to bottom
+      tunnelShadowWrapper.style.transform = `translateY(${
+        (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
+      }px)`;
+      // tunnel shadow go to top
+      introBgTunnelShadow.style.transform = `translateY(-${
+        (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
+      }px)`;
+      // decrease tunnelShadowWrapper height because it's will be climb out of the page
+      tunnelShadowWrapper.style.height = `${
+        wrapperHeight - (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
+      }px`;
+    }
   }
 });
