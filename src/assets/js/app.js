9a300c4e6f1c0bf6e8cded7ed60c96a2b1134cc6
Dima <dimafrontbackdev@gmail.com>
2022-12-07 17:53:21 +0200

    qw

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,54 @@
+function scrollBellowFooter(footerRectTop, footerHeight, wrapper, shadowRectTop, delay, shadow) {
+  if (-(footerRectTop - window.scrollY - window.innerHeight) - footerHeight > 0) {
+    wrapper.style.transform = `translateY(${
+      (shadowRectTop - window.scrollY - window.innerHeight + delay) * -1 +
+      wrapperHeight -
+      (shadowRectTop - window.scrollY - window.innerHeight + delay) * -1
+    }px)`;
+    // tunnel shadow go to top
+    shadow.style.transform = `translateY(-${
+      (shadowRectTop - window.scrollY - window.innerHeight + delay) * -1 +
+      wrapperHeight -
+      (shadowRectTop - window.scrollY - window.innerHeight + delay) * -1
+    }px)`;
+
+    wrapper.style.height = `0px`;
+  }
+}
+
+function shadowAnimation(rectTop, delay, wrapper, shadow) {
+  if (-(rectTop - window.scrollY - window.innerHeight) - delay > 0) {
+    // tunnel shadow wrapper go to bottom
+    wrapper.style.transform = `translateY(${
+      (rectTop - window.scrollY - window.innerHeight + delay) * -1
+    }px)`;
+    // tunnel shadow go to top
+    shadow.style.transform = `translateY(-${
+      (rectTop - window.scrollY - window.innerHeight + delay) * -1
+    }px)`;
+    // decrease tunnelShadowWrapper height because it's will be climb out of the page
+    wrapper.style.height = `${
+      wrapperHeight - (rectTop - window.scrollY - window.innerHeight + delay) * -1
+    }px`;
+    // if we scroll below footer, we hide shadow
+    if (-(footerRectTop - window.scrollY - window.innerHeight) - footerHeight > 0) {
+      tunnelShadowWrapper.style.transform = `translateY(${
+        (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1 +
+        wrapperHeight -
+        (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
+      }px)`;
+      // tunnel shadow go to top
+      introBgTunnelShadow.style.transform = `translateY(-${
+        (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1 +
+        wrapperHeight -
+        (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
+      }px)`;
+
+      tunnelShadowWrapper.style.height = `0px`;
+    }
+  }
+}
+
 window.addEventListener('load', () => {
   // for proper work of script of animation of disappear tunnel shadow
   window.scrollTo(0, 0);
