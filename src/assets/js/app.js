f074b86b0e107055b541469a341041e289ebd69d
Dima <dimafrontbackdev@gmail.com>
2022-12-11 14:31:14 +0200

    finish (or no)

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,54 +1,3 @@
-// function scrollBellowFooter(footerRectTop, footerHeight, wrapper, shadowRectTop, delay, shadow) {
-//   if (-(footerRectTop - window.scrollY - window.innerHeight) - footerHeight > 0) {
-//     wrapper.style.transform = `translateY(${
-//       (shadowRectTop - window.scrollY - window.innerHeight + delay) * -1 +
-//       wrapperHeight -
-//       (shadowRectTop - window.scrollY - window.innerHeight + delay) * -1
-//     }px)`;
-//     // tunnel shadow go to top
-//     shadow.style.transform = `translateY(-${
-//       (shadowRectTop - window.scrollY - window.innerHeight + delay) * -1 +
-//       wrapperHeight -
-//       (shadowRectTop - window.scrollY - window.innerHeight + delay) * -1
-//     }px)`;
-
-//     wrapper.style.height = `0px`;
-//   }
-// }
-
-// function shadowAnimation(rectTop, delay, wrapper, shadow) {
-//   if (-(rectTop - window.scrollY - window.innerHeight) - delay > 0) {
-//     // tunnel shadow wrapper go to bottom
-//     wrapper.style.transform = `translateY(${
-//       (rectTop - window.scrollY - window.innerHeight + delay) * -1
-//     }px)`;
-//     // tunnel shadow go to top
-//     shadow.style.transform = `translateY(-${
-//       (rectTop - window.scrollY - window.innerHeight + delay) * -1
-//     }px)`;
-//     // decrease tunnelShadowWrapper height because it's will be climb out of the page
-//     wrapper.style.height = `${
-//       wrapperHeight - (rectTop - window.scrollY - window.innerHeight + delay) * -1
-//     }px`;
-//     // if we scroll below footer, we hide shadow
-//     if (-(footerRectTop - window.scrollY - window.innerHeight) - footerHeight > 0) {
-//       tunnelShadowWrapper.style.transform = `translateY(${
-//         (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1 +
-//         wrapperHeight -
-//         (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
-//       }px)`;
-//       // tunnel shadow go to top
-//       introBgTunnelShadow.style.transform = `translateY(-${
-//         (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1 +
-//         wrapperHeight -
-//         (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
-//       }px)`;
-
-//       tunnelShadowWrapper.style.height = `0px`;
-//     }
-//   }
-// }
-
 window.addEventListener('load', () => {
   // for proper work of script of animation of disappear tunnel shadow
   window.scrollTo(0, 0);
@@ -60,20 +9,9 @@ window.addEventListener('load', () => {
   const tunnelShadowRectTop = introBgTunnelShadow.getBoundingClientRect().top; // position of tunnel in the page
   const footerRectTop = footer.getBoundingClientRect().top; // position of introRow in the page
   const delay = window.innerWidth > 1441 ? 300 : window.innerWidth > 1025 ? 200 : 100; // delay before starting animation
-  let wrapperHeight = introBgTunnelShadow.clientHeight;
   let footerHeight = footer.clientHeight;
-  let windowScrollYMorePrevValue = 0;
+  let windowScrollYMorePrevValue = 0; // for check if we scroll down
   let introBgTunnelShadowHeight = window.innerWidth;
-
-  const introBgStonesShadow = document.querySelector('.intro__bg-stones-shadow');
-
-  // const stonesShadowRectTop = introBgStonesShadow.getBoundingClientRect().top; // position of tunnel in the page
-  // let stonesWrapperHeight = introBgStonesShadow.clientHeight;
-
-  // initial height for tunnelShadowWrapper
-  // tunnelShadowWrapper.style.height = `${
-  //   wrapperHeight - (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
-  // }px`;
 
   // initial height for shadow
   introBgTunnelShadow.style.height = `${introBgTunnelShadowHeight}px`;
@@ -89,31 +27,22 @@ window.addEventListener('load', () => {
           (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
         }px)`;
 
+        // decrease tunnel shadow height
         introBgTunnelShadow.style.height = `${
           introBgTunnelShadowHeight -
           (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
         }px`;
 
-        // introBgTunnelShadow.style.transform = `translateY(${
-        //   (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
-        // }px)`;
-
         // if we scroll below footer, we hide shadow
-        // if (-(footerRectTop - window.scrollY - window.innerHeight) - footerHeight > 0) {
-        //   tunnelShadowWrapper.style.transform = `translateY(${
-        //     (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1 +
-        //     wrapperHeight -
-        //     (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
-        //   }px)`;
-        //   // tunnel shadow go to top
-        //   introBgTunnelShadow.style.transform = `translateY(-${
-        //     (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1 +
-        //     wrapperHeight -
-        //     (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
-        //   }px)`;
+        if (-(footerRectTop - window.scrollY - window.innerHeight) - footerHeight > 0) {
+          introBgTunnelShadow.style.transform = `translateY(${
+            (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1 +
+            introBgTunnelShadowHeight -
+            (tunnelShadowRectTop - window.scrollY - window.innerHeight + delay) * -1
+          }px)`;
 
-        //   tunnelShadowWrapper.style.height = `0px`;
-        // }
+          introBgTunnelShadow.style.height = `0px`;
+        }
       }
     }
   });
